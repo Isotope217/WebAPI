b04465051e3eddd996e5ffdf0aef367f79163034
gregh <gregholley@hotmail.com>
2021-09-01 19:50:11 +0100

    Added Fruit endpoint.

---
+++ WebAPI/Controllers/FruitController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        [HttpGet("GetFruit")]
+        public ActionResult<List<String>> GetFruit()
+        {
+            List<string> fruit = new List<string>
+            {
+                "Apple",
+                "Pear",
+                "Mango",
+                "Strawberry",
+            };
+
+            return fruit;
+        }
+    }
+}
